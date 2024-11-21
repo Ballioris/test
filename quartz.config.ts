1a420b01c3c5b9c095c4958927719ba76af464f6
Ballioris <dusek.zbynek2@gmail.com>
2024-11-21 20:36:48 +0100

    Quartz sync: Nov 21, 2024, 8:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🌟Altera🌟",
     pageTitleSuffix: "| Bali vás má rád",
     enableSPA: true,
     enablePopovers: true,
